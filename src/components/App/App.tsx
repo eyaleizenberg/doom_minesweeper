186ae26e033728f6141b2910416f9c6e54064c03
Eyal Eizenberg <eyale@wix.com>
2018-05-02 13:56:47 +0300

    remove wrong line

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,8 +3,6 @@ import MainContainer from '../../containers/MainContainer';
 
 export default class Minesweeper extends React.PureComponent {
   componentDidMount() {
-    document.domain = 'github.io';
-
     window.postMessage(
       'gameLoaded',
       'https://wix-incubator.github.io/mastermind'
