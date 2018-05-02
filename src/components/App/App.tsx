9f49afb956f824d0766e52bc898809d71ba1e4d4
Eyal Eizenberg <eyale@wix.com>
2018-05-02 13:31:59 +0300

    post message when loaded

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import MainContainer from '../../containers/MainContainer';
 
 export default class Minesweeper extends React.PureComponent {
+  componentDidMount() {
+    window.postMessage(
+      'gameLoaded',
+      'https://wix-incubator.github.io/mastermind'
+    );
+  }
+
   render() {
     return <MainContainer />;
   }
