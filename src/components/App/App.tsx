1d47abbda358ad25ec73fb931c53a1b1c5bd53a8
Eyal Eizenberg <eyale@wix.com>
2018-05-02 13:51:56 +0300

    set the domain

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,8 @@ import MainContainer from '../../containers/MainContainer';
 
 export default class Minesweeper extends React.PureComponent {
   componentDidMount() {
+    document.domain = 'github.io';
+
     window.postMessage(
       'gameLoaded',
       'https://wix-incubator.github.io/mastermind'
